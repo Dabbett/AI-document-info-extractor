5d9b530b9c48b846d14fed56f18391a426b1852a
Dylan Abbett <abbettdylan@gmail.com>
2024-04-30 17:07:31 -0400

    add drag and drop file functionality

--- src/app/quiz/UploadDoc.tsx
+++ src/app/quiz/UploadDoc.tsx
@@ -18,7 +18,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     <div className="w-full">
         <form className="w-full" onSubmit={handleSubmit}>
             <label htmlFor="document" className="bg-secondary w-full flex h-20 rounded-md border-2 border-dotted border-blue-900 relative">
-                <div className="absolute inset-0 m-auto flex justify-center items-center"> Drag a file</div>
+                <div className="absolute inset-0 m-auto flex justify-center items-center"> {document && document?.name ? document.name: "Drag a file"}</div>
                 <input type="file" id="document" className=" relative block w-full h-full z-50 opacity-0 mt-2" onChange={(e) => setDocument(e?.target?.files?.[0])}/>
             </label>
             <Button size="lg" className="mt-2" type="submit">Generate Quiz</Button>
