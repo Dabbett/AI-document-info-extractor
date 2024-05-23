18c32070a0f22b460e4d9ff3a97787ce3cb07b78
Dylan Abbett <abbettdylan@gmail.com>
2024-05-23 14:17:24 -0400

    edit drag and drop area

--- src/app/quiz/UploadDoc.tsx
+++ src/app/quiz/UploadDoc.tsx
@@ -49,7 +49,7 @@ const UploadDoc = () => {
       {isLoading ? <Spinner text="Loading... This may take a minute" /> : <form className="w-full" onSubmit={handleSubmit}>
         <label htmlFor="document" className="bg-secondary w-full flex h-20 rounded-md border-4 border-dashed border-blue-900 relative">
           <div className="absolute inset-0 m-auto flex justify-center items-center">
-            {document && document?.name ? document.name : "Drag a file"}</div>
+            {document && document?.name ? document.name : "Click Here or Drag a file"}</div>
           <input type="file" id="document" className="relative block w-full h-full z-50 opacity-0" onChange={handleDocumentUpload} />
         </label>
         <p className="text-secondary-foreground my-2">Supported file types: pdf</p>
